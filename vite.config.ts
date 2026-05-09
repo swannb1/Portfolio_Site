d317df2bd41bf4b40233acce77fb8b7112a3054c
swannb1 <161678870+swannb1@users.noreply.github.com>
2026-05-09 04:34:05 -0600

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Portfolio_Site",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
